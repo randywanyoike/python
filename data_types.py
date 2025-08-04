befab61b852bd2acebc5203eec2e61dbf0d3abdb
randywanyoike <randywanyoike@gmail.com>
2025-08-04 15:59:44 +0300

    completed datatypes

--- data_types.py
+++ data_types.py
@@ -1,4 +1,20 @@
 x=10
+print(x)
 myName="Randy"
+print(myName)
 is_happy=True
+print(is_happy)
 no_value=None
+print(no_value)
+
+#Advanced Data Types
+my_list=[1, 2, 3, 4, "string",True]#array
+print(my_list)
+my_set={1, 2, 3, 4, "string", True}#no duplicate
+print(my_set)
+my_tuple=(1, 2, 3, 4, "string", True)
+print(my_tuple)
+my_obj={"key":"value","age":30}#key ->value be in pairs 
+print(my_obj)
+
+#Printing the data types/all values 
