79538c86ee9d238e29db0fa1092ff59de054ee6e
randywanyoike <randywanyoike@gmail.com>
2025-08-04 16:40:39 +0300

    complete

---
+++ conditions.py
@@ -0,0 +1,18 @@
+age = 20
+
+if age>21:
+    print("You can drink")
+else:
+    print("You are underage")
+
+if age<12:
+    print("You are a child")
+elif age>=12 and age<18:
+    print("You are a teenager")
+elif age>=18 and age<21:
+    print("Imagine people were drinking")
+elif age>=21:
+    print("Legal drinking age")
+else :
+    print("Invalid age")
+    
---
+++ loop.py
@@ -0,0 +1,22 @@
+to_loop=True
+i=0
+
+while to_loop:
+    if i>10:
+        to_loop=False
+    print("i is",i)
+    i=i+1
+
+
+    k=0
+    while k<10:
+        print("k is", k)
+        k=k+1
+
+
+for i in range(2,10):
+    print("For loop is", i)
+
+
+for i in range(10,2,-1):
+    print("For loop is", i)
